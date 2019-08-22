0e027fd8cfc867f14464dfbe521f9dd3e97cfe31
Henry Neal <henry.neal0@gmail.com>
2019-08-22 16:13:16 -0400

    attributes and scopes done

--- spec/models/employee_spec.rb
+++ spec/models/employee_spec.rb
@@ -31,9 +31,40 @@ RSpec.describe Employee, type: :model do
   end
 
   describe "Attributes" do 
+    it "has expected attributes" do
+      employee = Employee.new(first_name:"Henry", last_name:"Neal", rewards_balance: 500)
+      result = employee.attribute_names.map(&:to_sym) #{|name| name.to_sym}
+
+      expect(result).to contain_exactly(
+        :id,
+        :first_name,
+        :last_name,
+        :rewards_balance,
+        :created_at,
+        :updated_at
+      )
+    end
   end
 
   describe "Scopes" do
+    describe ".zero_balance" do
+      before do
+        Employee.create!([
+          {first_name:"A", last_name:"Mike", rewards_balance: 0},
+          {first_name:"B", last_name:"Sue", rewards_balance: 0},
+          {first_name:"C", last_name:"Neal", rewards_balance: 100}
+        ])
+      end
+
+      it "returns list of active records" do
+        results = Employee.zero_balance
+
+        expect(results.count).to eq 2
+        expect(results.first.first_name).to eq "A"
+        expect(results.last.first_name).to eq "B"
+        expect(results.any? { |employee| employee.first_name == "C" }). to be false
+      end
+    end
   end
 
   describe "Instance Variables" do
